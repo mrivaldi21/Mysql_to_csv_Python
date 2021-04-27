72ecb48eb1be59a5b3f88a1eada2f8ef6f5b458e
Rivaldi <43880909+mrivaldi21@users.noreply.github.com>
2021-04-27 23:46:45 +0700

    update01

--- main.py
+++ main.py
@@ -1,35 +1,7 @@
 import pandas
-# import pyodbc
 import MySQLdb as dbapi
-import sys
-import csv
-import pandas as pd
 
-# conn = pyodbc.connect('Driver={SQL Server};'
-#                       'Server=localhost;'
-#                       'Database=quiz;'
-#                       'Trusted_Connection=yes;')
-# sql_query = pd.read_sql_query('''
-#                                 select * from quiz'''
-#                               , conn)
-# df = pd.DataFrame(sql_query)
-# df.to_csv("datas.csv")
-
-
-# QUERY = 'SELECT * FROM quiz.quiz;'
-# db = dbapi.connect(host='localhost', db='quiz', user='root', passwd='')
-#
-# cur = db.cursor()
-# cur.execute(QUERY)
-# result = cur.fetchall()
-#
-# c = csv.writer(open('dbdump01.csv', 'w'))
-# for x in result:
-#     c.writerow(x)
-
-#################################################
-
-QUERY = 'SELECT * FROM quiz.quiz;'
+QUERY = 'SELECT * FROM quiz;'
 db = dbapi.connect(host='localhost', db='quiz', user='root', passwd='')
 
 cur = db.cursor()
